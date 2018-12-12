d7b9946fb118c5c30617cdc13e1ce4152e67494c
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2018-12-12 17:06:47 +0100

    creation xml et proc

--- server.php
+++ server.php
@@ -1,15 +1,60 @@
 <?php
 
-$opts = array('http' => array(/*'proxy'=> 'tcp://127.0.0.1:8080',*/ 'request_fulluri'=> true));
+$opts = array('http' => array('proxy'=> 'tcp://127.0.0.1:8080', 'request_fulluri'=> true));
 $context = stream_context_create($opts);
 
-//Call API météo
-$param_auth = 'ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2';
+/**
+ * Calls the meteo API 
+ * @param localisation 
+ *      Localisation of the client to insert in url
+ * @param auth 
+ *      Authentification key to insert in url
+ * @return 
+ *      True : xml file has been created with success
+ *      False : API didn't respond
+ */
+function callMeteo($localisation, $auth){
+    $success = false;
+    $url_meteo = "http://www.infoclimat.fr/public-api/gfs/xml?_ll=$localisation&_auth=$auth";
+    $meteo_data = file_get_contents($url_meteo/*, false, $context*/);
+    if($meteo_data){
+        
+        /*$args = ['/_xml' => $xml];
+        $xsltp = xslt_create();
+        xslt_set_encoding($xsltp, 'UTF-8');
+        $html = xslt_process($xsltp, 'arg:/_xml', './meteo.xsl', null, $args);*/
+        $xml_file = fopen('meteo.xml', 'w');
+        fwrite($xml_file, $meteo_data);
+        $xml = new DOMDocument();
+        $xml->load('meteo.xml');
+        $xsl = new DOMDocument();
+        $xsl->load('meteo.xsl');
+        $proc = new XSLTProcessor();
+        $proc->importStylesheet($xsl);
+        echo $proc->transformToDoc($xml);
+        $success = true;
+    } else {
+        $success = false;
+    }
+    return $success;
+}
+
 $param_localisation = '48.67103,6.15083';
-$url_meteo = "http://www.infoclimat.fr/public-api/gfs/xml?_ll=$param_localisation&_auth=$param_auth";
-$meteo_data = simplexml_load_string(file_get_contents($url_meteo));
+$param_auth = 'ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2';
+callMeteo($param_localisation, $param_auth);
+
+
+/*
+
+$html = <<<END
+    <!DOCTYPE
+    <html>
+    
+    </html>
+END;
+
 if($meteo_data){
-    //Print météo
+    
 } else {
     //Météo indisponible
 }
@@ -33,4 +78,4 @@ if($parkings_data){
     } 
 } else {
     //Parkings indisponibles
-}
+}*/
