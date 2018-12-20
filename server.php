66cf34fc9ed8b192c6b858d2784fc1d7ef43225a
Anthony Zink <anthony.zink@outlook.fr>
2018-12-20 20:47:31 +0100

    c'est le mien le best

--- server.php
+++ server.php
@@ -1,4 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+
+/*
+$default_opts = array(
+    'http'=>array(
+        'proxy'=> 'tcp://www-cache.iutnc.univ-lorraine.fr:3128/',
+        'request_fulluri'=> true
+    )
+  );
+
+$default = stream_context_set_default($default_opts);
+*/
+
 /**
  * Calls the meteo API 
  * @param localisation 
@@ -10,41 +23,84 @@
  *      False : API didn't respond
  */
 function callMeteo($localisation, $auth){
-    $opts = array('http' => array(/*'proxy'=> 'tcp://www-cache.iutnc.univ-lorraine.fr:3128/',*/'request_fulluri'=> true));
-
-    $context = stream_context_create($opts);
-    $success = false;
     $url_meteo = "http://www.infoclimat.fr/public-api/gfs/xml?_ll=$localisation&_auth=$auth";
-    $meteo_data = file_get_contents($url_meteo, false, $context);
+    $meteo_data = new SimpleXMLElement(file_get_contents($url_meteo, false));
     if($meteo_data){
-        
-        
-        $xml_file = fopen('meteo.xml', 'w');
-        fwrite($xml_file, $meteo_data);
-        fclose($xml_file);
-        $xml = new DOMDocument();
-        $xml->load('meteo.xml');
-
         $xsl = new DOMDocument();
         $xsl->load('meteo.xsl');
         $proc = new XSLTProcessor();
         $proc->importStylesheet($xsl);
        
-        $meteo = $proc->transformToXML($xml);
+        $meteo = $proc->transformToXML($meteo_data);
         return '
             <div class="meteo">
             '.$meteo.'
             </div>
         '; 
     } else {
-        var_dump('aucune donnée meteo');die;
+        return "<div class='meteo'>Pas de meteo :'(</div>";
     }
 }
+
+function getIp(){
+    $url_ip = "http://api.ipstack.com/check?access_key=1c2c31a9132584a1c622b7e1bf032084";
+$ip_data =  json_decode(file_get_contents($url_ip, false));
+    return [$ip_data->longitude, $ip_data->latitude];
+}
+
+function getVelos(){
+    $url_velos = "http://www.velostanlib.fr/service/carto";
+    $stations =  new SimpleXMLElement(file_get_contents($url_velos));
+
+    $script = "";
+    $html = "";
+    foreach ($stations->markers->marker as $station) {
+        $infos =  new SimpleXMLElement(file_get_contents("http://www.velostanlib.fr/service/stationdetails/nancy/".$station->attributes()->number));
+        $free = json_decode($infos->free);
+        $total = json_decode($infos->total);
+        $lat = $station->attributes()->lat;
+        $lng = $station->attributes()->lng;
+        $name = $station->attributes()->name;
+        $script .= <<<END
+        L.marker([$lat,$lng ],{
+            icon
+        }).addTo(map).bindPopup("<h2>$name</h2><p style='text-align:center'>Vélos disponibles :$free/$total</p>", {closeOnClick: true, autoClose: true});
+END;
+    
+    }
+    return $script;
+}
+
 
 
 $param_localisation = '48.67103,6.15083';
 $param_auth = 'ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2';
-//
+
+$script = "
+    let map = L.map('mapid',{
+        center : [".getIp()[1].", ".getIp()[0]."],
+        zoom : 17
+    });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: \"Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>\",
+        maxZoom: 25,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiYW50aG9ueXppbmsiLCJhIjoiY2pwb2g2YXpkMDB6OTN4cWZvdTF3cGljZiJ9.ETkoyTeCMRTRX2SAc0TrXg'
+    }).addTo(map);
+
+    L.marker([".getIp()[1].", ".getIp()[0]."],{
+        opacity : 1
+    }).addTo(map);
+
+    let icon = L.icon({
+        iconUrl: 'assets/icon.png',
+        iconSize: [38, 38],
+    });
+
+    ".getVelos()."
+";
+
 
 $html = "
 <html>
@@ -62,7 +118,9 @@ $html = "
         </div>
 
         <script src='https://code.jquery.com/jquery-3.3.1.min.js'integrity='sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8='crossorigin='anonymous'></script>
-        <script src='assets/map.js'></script>
+        <script>
+        ". $script ."
+        </script>
     </body>
     </html>
 ";
